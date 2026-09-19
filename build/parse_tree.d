057753bb36b0319d2e4ed8a5348773cabe880192
Daniel Inhoi Kim <dkim311006@gmail.com>
2026-09-19 10:06:56 -0400

    Delete build directory

--- build/parse_tree.d
+++
@@ -1,8 +0,0 @@
-build/parse_tree.o: src/parse_tree.c include/parse_tree.h \
- include/tokenization.h include/string_view.h include/dynamic_array.h \
- include/tokenization.h
-include/parse_tree.h:
-include/tokenization.h:
-include/string_view.h:
-include/dynamic_array.h:
-include/tokenization.h:
