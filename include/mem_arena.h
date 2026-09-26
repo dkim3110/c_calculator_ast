c48fdb35fca1b7b870aed95fccf9cb8bea239bff
Daniel Inhoi Kim <dkim311006@gmail.com>
2026-09-27 00:43:12 +0900

    Reorginize mem_arena.h

--- include/mem_arena.h
+++ include/mem_arena.h
@@ -18,6 +18,13 @@
 #define MiB(n) ((size_t)(n) << 20)
 #define GiB(n) ((size_t)(n) << 30)
 
+#define arena_new()														arena_init(GiB(1), MiB(2))
+#define arena_clear(arena)										arena_clear_impl((arena), false)
+#define arena_hard_clear(arena)								arena_clear_impl((arena), true)
+#define arena_alloc(arena, type_size)					arena_allocation_impl((arena), (type_size), false)
+#define arena_calloc(arena, count, type_size) arena_allocation_impl((arena), ((type_size) * (count)), true)
+#define arena_getpos(arena)										((arena)->pos)
+
 typedef struct {
 	size_t reserve;
 	size_t commit;
@@ -25,100 +32,28 @@ typedef struct {
 	size_t commit_pos;
 } mem_arena;
 
-// == FUNCTION DECLARATIONS ===================================================
+static inline mem_arena *arena_init(size_t reserve_size, size_t commit_size);
+static inline void arena_destroy(mem_arena *arena);
+static inline void arena_clear_impl(mem_arena *arena, bool do_decommit);
+static inline void *arena_allocation_impl(mem_arena *arena, size_t size, bool zero_out);
+static inline void arena_pop(mem_arena *arena, size_t pos);
+
+static inline uint32_t plat_get_pagesize(void);
+static inline void *plat_mem_reserve(size_t size);
+static inline bool plat_mem_commit(void *ptr, size_t size);
+static inline bool plat_mem_decommit(void *ptr, size_t size);
+static inline bool plat_mem_release(void *ptr, size_t size);
+
+// == FUNCTION DEFINITIONS ====================================================
 
 /*
  * Function to create and initialize an arena based on the reserve and commit size provided.
- * If `commit_size` is less than the system page size, initializes it to be equal to the page size.
- * The macro `arena_new()` is provided for ease of use, setting `reserve_size` and `commit_size` to a default of 1
+ * If commit_size is less than the system page size, initializes it to be equal to the page size.
+ * The macro arena_new() is provided for ease of use, setting reserve_size and commit_size to a default of 1
  * GiB and 2 MiB each.
- * @param: sizes of virtual memory to reserve and to commit at a time.
- * @return: pointer to the allocated `mem_arena struct`; NULL in case of failure.
+ * Returns a pointer to the allocated mem_arena struct or NULL if failure.
  */
 
-static inline mem_arena *arena_init(size_t reserve_size, size_t commit_size);
-#define arena_new() arena_init(GiB(1), MiB(2))
-
-/*
- * Function to destroy an arena.
- * @param: pointer to the allocated mem_arena struct.
- */
-
-static inline void arena_destroy(mem_arena *arena);
-
-/*
- * Function to clear an arena. If `do_decommit` is set to true, decommits all pages except the first if commit position
- * is greater than page size.
- * The macros `arena_clear()` and `arena_hard_clear()` are provided for ease of use.
- * @param: pointer to the allocated mem_arena struct, boolean representing if it should decommit.
- */
-
-static inline void arena_clear_impl(mem_arena *arena, bool do_decommit);
-#define arena_clear(arena)			arena_clear_impl((arena), false)
-#define arena_hard_clear(arena) arena_clear_impl((arena), true)
-
-/*
- * Function to push an element to an arena based on the size provided.
- * The macros `arena_alloc()` and `arena_calloc()` are provided for ease of use.
- * @param: pointer to the allocated mem_arena struct, size of the element to push, boolean representing if it should
- * zero out the memory.
- * @return: pointer to the pushed element; NULL in case of failure.
- */
-
-static inline void *arena_allocation_impl(mem_arena *arena, size_t size, bool zero_out);
-#define arena_alloc(arena, type_size)					arena_allocation_impl((arena), (type_size), false)
-#define arena_calloc(arena, count, type_size) arena_allocation_impl((arena), ((type_size) * (count)), true)
-
-/*
- * Function to pop an element from an arena to the given position. Does not decommit memory.
- * The macro `arena_getpos()` is provided for ease of use.
- * @param: pointer to the allocated mem_arena struct, position to pop the element to.
- */
-
-static inline void arena_pop(mem_arena *arena, size_t pos);
-#define arena_getpos(arena) ((arena)->pos)
-
-/*
- * Function to get the size of the system's page size. Direct call of this function is disadviced.
- * @return: size of page in bytes.
- */
-
-static inline uint32_t plat_get_pagesize(void);
-
-/*
- * Function to reserve memory based on the size provided. Direct call of this function is disadviced.
- * @param: size of memory in bytes.
- * @return: pointer to the reserved memory; NULL in case of failure.
- */
-
-static inline void *plat_mem_reserve(size_t size);
-
-/*
- * Function to commit a portion of reserved virtual memory. Direct call of this function is disadviced.
- * @param: pointer to virual address, size to commit in bytes.
- * @return: true if successful, false if failure.
- */
-
-static inline bool plat_mem_commit(void *ptr, size_t size);
-
-/*
- * Function to decommit a portion of committed virtual memory. Direct call of this function is disadviced.
- * @param: pointer to virual address, size to decommit in bytes.
- * @return: true if successful, false if failure.
- */
-
-static inline bool plat_mem_decommit(void *ptr, size_t size);
-
-/*
- * Function to release a portion of reserved virtual memory. Direct call of this function is disadviced.
- * @param: pointer to virual address, size to release in bytes.
- * @return: true if successful, false if failure.
- */
-
-static inline bool plat_mem_release(void *ptr, size_t size);
-// =================================================== FUNCTION DECLARATIONS ==
-
-// == FUNCTION DEFINITIONS ====================================================
 static inline mem_arena *arena_init(size_t reserve_size, size_t commit_size) {
 	uint32_t page_size = plat_get_pagesize();
 	if ((page_size & (page_size - 1)) != 0) return NULL;
@@ -143,6 +78,13 @@ static inline mem_arena *arena_init(size_t reserve_size, size_t commit_size) {
 
 	return arena;
 } /* arena_init() */
+
+/*
+ * Function to push an element to an arena based on the size provided. Zeroes out memory based on
+ * zero_out's value.
+ * The macros arena_alloc() and arena_calloc() are provided for ease of use.
+ * Returns a pointer to the allocated element or NULL if failure.
+ */
 
 static inline void *arena_allocation_impl(mem_arena *arena, size_t size, bool zero_out) {
 	size_t pos_align = ALIGN_UP_POW2(arena->pos, ARENA_ALIGN);
@@ -170,12 +112,23 @@ static inline void *arena_allocation_impl(mem_arena *arena, size_t size, bool ze
 	return out;
 } /* arena_allocation_impl() */
 
+/*
+ * Function to pop an element from an arena to the given position. Does not decommit memory.
+ * The macro arena_getpos() is provided for ease of use.
+ */
+
 static inline void arena_pop(mem_arena *arena, size_t pos) {
 	size_t new_pos = (pos < ARENA_BASE_POS) ? ARENA_BASE_POS : pos;
 	if (new_pos < arena->pos) {
 		arena->pos = new_pos;
 	}
 } /* arena_pop() */
+
+/*
+ * Function to clear an arena. If do_decommit is set to true, decommits all pages except the first if commit position
+ * is greater than page size.
+ * The macros arena_clear() and arena_hard_clear() are provided for ease of use.
+ */
 
 static inline void arena_clear_impl(mem_arena *arena, bool do_decommit) {
 	if (do_decommit) {
@@ -192,6 +145,10 @@ static inline void arena_clear_impl(mem_arena *arena, bool do_decommit) {
 	arena_pop(arena, ARENA_BASE_POS);
 } /* arena_clear_impl() */
 
+/*
+ * Function to destroy an arena and release its memory.
+ */
+
 static inline void arena_destroy(mem_arena *arena) {
 	if (arena) plat_mem_release(arena, arena->reserve);
 } /* arena_destroy() */
@@ -203,24 +160,51 @@ static inline void arena_destroy(mem_arena *arena) {
 #include <sys/mman.h>
 #include <unistd.h>
 
+/*
+ * Function to get the size of the system's page size. Direct call of this function is disadviced.
+ */
+
 static inline uint32_t plat_get_pagesize(void) {
 	static uint32_t page_size_cache = 0;
 	if (!page_size_cache) page_size_cache = (uint32_t)sysconf(_SC_PAGESIZE);
 	return page_size_cache;
 } /* plat_get_pagesize() */
 
+/*
+ * Function to reserve memory based on the size provided. Direct call of this function is disadviced.
+ * Returns a pointer to the reserved memory or NULL if failure.
+ */
+
 static inline void *plat_mem_reserve(size_t size) {
 	void *out = mmap(NULL, size, PROT_NONE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 	return (out == MAP_FAILED) ? NULL : out;
 } /* plat_mem_reserve() */
 
+/*
+ * Function to commit a portion of reserved virtual memory. Direct call of this function is disadviced.
+ * Accepts a pointer to virual address and size to commit in bytes.
+ * Returns true if successful, false if failure.
+ */
+
 static inline bool plat_mem_commit(void *ptr, size_t size) {
 	return (mprotect(ptr, size, PROT_READ | PROT_WRITE) == 0);
 } /* plat_mem_commit() */
 
+/*
+ * Function to decommit a portion of committed virtual memory. Direct call of this function is disadviced.
+ * Accepts a pointer to virual address and size to decommit in bytes.
+ * Returns true if successful, false if failure.
+ */
+
 static inline bool plat_mem_decommit(void *ptr, size_t size) {
 	return (mprotect(ptr, size, PROT_NONE) != 0) ? false : (madvise(ptr, size, MADV_DONTNEED) == 0);
 } /* plat_mem_decommit() */
+
+/*
+ * Function to release a portion of reserved virtual memory. Direct call of this function is disadviced.
+ * Accepts a pointer to virual address and size to release in bytes.
+ * Returns true if successful, false if failure.
+ */
 
 static inline bool plat_mem_release(void *ptr, size_t size) {
 	return (munmap(ptr, size) == 0);
